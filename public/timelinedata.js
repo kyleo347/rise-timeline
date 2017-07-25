9c6c78f6f3c1d6fe839c8717832eb67108001ab5
Kyleo <kyleoconnor347@gmail.com>
2017-07-25 21:02:42 +0000

    Working with 2nd version edits

---
+++ public/timelinedata.js
@@ -0,0 +1,334 @@
+timelineData = {
+    "title": {
+        "media": {
+          "url": "./img/Rise.png",// "//www.flickr.com/photos/tm_10001/2310475988/",
+          "caption": "",// "Whitney Houston performing on her My Love is Your Love Tour in Hamburg.",
+          "credit": "",// "flickr/<a href='http://www.flickr.com/photos/tm_10001/'>tm_10001</a>"
+        },
+        "text": {
+          "headline": "Rise Movement",
+          "text": ""
+        }
+    },
+    "events": [
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "11",
+          //"day": "9",
+          "year": "2014"
+        },
+        "text": {
+          "headline": "Rise is founded.",
+          "text": ""
+        },
+        "background": {
+          //"url": "./img/Rise.png"
+          "color": "#ea6907"
+        },
+        "group": "Rise",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "1",
+          "year": "2015"
+        },
+        "text": {
+          "headline": "Massachusetts bill introduced.",
+          "text": ""
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "1",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Federal bill introduced to the Senate. ",
+          "text": ""
+        },
+        "group": "Federal",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "6",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Federal bill introduced to the House.",
+          "text": ""
+        },
+        "group": "Federal",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "8",
+          "day": "4",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Massachusetts bill passed House.",
+          "text": ""
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "9",
+          "day": "8",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Massachusetts bill passed Senate.",
+          "text": ""
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "10",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Federal bill signed into law.",
+          "text": ""
+        },
+        "group": "Federal",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "10",
+          "day": "19",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Massachusetts bill signed into law.",
+          "text": ""
+        },
+        "group":"State",
+      },/*
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "1",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Sexual assault survivors' bill of rights introduced in states across country",
+          "text": `Virginia bill introduced. (1/11) <br>
+                  Washington bill introduced. (1/11) <br>
+                  Vermont bill introduced. (1/19) <br>
+                  Maine bill introduced. (1/19) <br>
+                  Maryland bill introduced. (1/23) <br>
+                  Utah bill introduced. (1/25) <br>
+                  Arizona bill introduced. (1/31)`
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "2",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "State Bills introduced",
+          "text": `Washington bill .2 introduced. (2/2) <br>
+                  Oklahoma bill introduced. (2/6) <br>
+                  Idaho bill introduced. (2/10) <br>
+                  California bill introduced. (2/17)`
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "2",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "State Legistatures pass bill",
+          "text": `Virginia bill passed House unanimously. (2/7) <br>
+                  Virginia bill passed Senate unanimously. (2/15) <br>
+                  Utah bill passed House. (2/17) <br>
+                  Vermont bill passed House. (2/17) <br>
+                  Arizona bill passed Senate unanimously. (2/21) <br>
+                  Idaho bill passed House unanimously. (2/27)`
+        },
+        "group":"State",
+      },*/
+      {
+        "media": { 
+          "url": "./img/RiseVA.jpg",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "3",
+          "day": "16",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Virginia bill signed into law.",
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "./img/RiseUT.png",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "3",
+          "day": "22",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Utah bill signed into law.",
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "4",
+          "day": "6",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Idaho bill signed into law.",
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "4",
+          "day": "18",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Maryland bill signed into law.",
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "5",
+          "day": "16",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Washington bill signed into law.",
+        },
+        "group":"State",
+      },
+      {
+        "media": { 
+          "url": "",
+          "caption": "",
+          "credit": "",
+        },
+        "start_date": {
+          "month": "5",
+          "day": "23",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Vermont bill signed into law.",
+        },
+        "group":"State",
+      },
+      /*
+      {
+        "media": { 
+        },
+        "start_date": {
+          "month": "",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "",
+          "text": ""
+        }
+      },
+      {
+        "media": { 
+        },
+        "start_date": {
+          "month": "",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "",
+          "text": ""
+        }
+      },*/
+    ]
+}
